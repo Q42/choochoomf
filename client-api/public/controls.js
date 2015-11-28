3e7aae43041c58645822f88f26b6b9aa91e1ccb4
tstikvoort <tobias@q42.nl>
2015-11-28 11:43:43 +0100

    added riding animation

--- client-api/public/controls.js
+++ client-api/public/controls.js
@@ -1,3 +1,18 @@
+function goTo(location) {
+  console.log('location',location);
+  animate();
+}
+
+function animate() {
+  var body = document.getElementsByTagName('body')[0];
+  // var initalClassList = body.classList;
+  body.classList.add('trainRiding');
+
+  setTimeout(function(){
+    body.classList.remove('trainRiding');
+  }, 2000);
+}
+
 function tF() {
   post('speed', {
     speed: 1
