67f8693ab9e7704b00c53e31cb1783887ca998a7
Jaap Mengers <jaapm@q42.nl>
2015-11-28 10:23:30 +0100

    Adds a locking construction to prevent both servo's being used at the same time

--- client-api/swivelapi.js
+++ client-api/swivelapi.js
@@ -1,20 +1,31 @@
 var piblaster = require('pi-blaster.js');
 
+var PAN = 'PAN';
+var TILT = 'TILT';
+
+var lock;
+
 module.exports = {
 
 	pan: function(value){
+		if(lock && lock != PAN){
+			return;
+		}
 
 		var newVal = previousX + (value * 0.1);
-
 		if(newVal > 1 || newVal < -1){
 			return;
 		}
 
 		setX(newVal);
-
 		previousX = newVal;
+		lock = PAN;
+		renewLock();
 	},
 	tilt: function(value){
+		if(lock && lock != TILT){
+			return;
+		}
 		
 		var newVal = previousY + (value * 0.1);
 
@@ -23,17 +34,34 @@ module.exports = {
 		}
 
 		setY(newVal);
-
 		previousY = newVal;
+		lock = TILT;
+		renewLock();
 	},
 	center: function(){
 		setX(0);
-		setY(0);
+		setTimeout(function(){
+			setY(0);
+		}, 2000);
 	}
 }
 
 var previousX = 0;
 var previousY = 0;
+
+var interval;
+
+function renewLock(){
+	if(interval)
+		clearInterval(interval);
+
+	interval = setInterval(releaseLock, 500);
+}
+
+function releaseLock(){
+	lock = undefined;
+	console.log('Releasing lock', lock);
+}
 
 function setX(val){
 	var x = computeX(val);
